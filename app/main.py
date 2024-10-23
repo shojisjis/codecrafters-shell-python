f766c13d17ef5d7ed95b77d9b0f81491c97de179
shojisjis <steve040315@gmail.com>
2024-10-23 13:25:31 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import sys
 
 
+# 유효한 명령어 목록 추가
+VALID_COMMANDS = ['exit', 'echo', 'type']
+
 def main():
     # Main loop
     while True:
@@ -11,19 +14,28 @@ def main():
         # 사용자 입력 대기
         cmd = input()
         parsed_cmd = parse_command(cmd)
+        
         if parsed_cmd['command'] == 'exit':
             sys.exit(int(parsed_cmd['args']))
         elif parsed_cmd['command'] == 'echo':
             print(parsed_cmd['args'])
+        elif parsed_cmd['command'] == 'type':
+            check_command(parsed_cmd['args'])
         else:
             print(f"{parsed_cmd['command']}: command not found")
 
 def parse_command(cmd):
     parts = cmd.split(maxsplit=1)
     command = parts[0]
-    args = parts[1] if len(parts) > 1 else ''
+    args = parts[1] if len(parts) > 1 else '0' if command == 'exit' else ''
     return {'command': command, 'args': args}
 
+# 'type' 명령어를 처리하는 함수 추가
+def check_command(cmd):
+    if cmd in VALID_COMMANDS:
+        print(f"{cmd} is a shell builtin")
+    else:
+        print(f"{cmd}: not found")
 
 if __name__ == "__main__":
     main()
