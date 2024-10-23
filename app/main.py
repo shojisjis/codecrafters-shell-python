91b63d7af2e28a46cd373c3ca789f1fdbbe9a670
shojisjis <steve040315@gmail.com>
2024-10-23 12:30:13 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,11 +10,19 @@ def main():
 
         # 사용자 입력 대기
         cmd = input()
-        if cmd.startswith("exit"):
-            exit_code = (cmd + " ").split(" ", 1)[1]
-            sys.exit(int(exit_code.strip()))
+        parsed_cmd = parse_command(cmd)
+        if parsed_cmd['command'] == 'exit':
+            sys.exit(int(parsed_cmd['args']))
+        elif parsed_cmd['command'] == 'echo':
+            print(parsed_cmd['args'])
         else:
-            print(f"{cmd}: command not found")
+            print(f"{parsed_cmd['command']}: command not found")
+
+def parse_command(cmd):
+    parts = cmd.split(maxsplit=1)
+    command = parts[0]
+    args = parts[1] if len(parts) > 1 else ''
+    return {'command': command, 'args': args}
 
 
 if __name__ == "__main__":
